50db5c4fe92a0ad36e5f2d2a2b8eebf2e7fb4263
Gerard Sans <gerard.sans@bigsystemstechnology.com>
2016-07-14 13:14:14 +0100

    Initial commit ^_^

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# fullstack-ng2-workshop
-Based on angular2-webpack-starter (webpack + TypeScript)
+#FullStack 2016 | Workshop - Introduction to Angular 2
+
+[Instructions and Setup](http://bit.ly/fullstack-ng2-doc)
+
+> Based on https://github.com/angular-class/angular2-webpack-starter.
+
+```
+ngconf-codelab/
+ ├──src/                       
+ |   ├──main.browser.ts        * entry file
+ │   │
+ |   ├──index.html             * index page
+ │   │
+ │   │
+ │   └──app/                   * basic application to explore Angular 2 features.
+```
+
+# License
+ [MIT](/LICENSE)
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,60 @@
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'contact',
+  styles: [`
+    .ng-valid[required] {
+      border: 2px solid #42A948; /* green */
+    }
+
+    .ng-invalid {
+      border: 2px solid #a94442; /* red */
+    }
+
+    .alert {
+      color: #a94442; /* red */
+    }
+  `],
+  template: `
+    <md-card>
+      <md-card-content>
+        <h1>Contact</h1>
+        <form #f="ngForm" (ngSubmit)="onSubmit(f.form.value)">
+          <p>
+            <label for="email">Email:</label>
+            <input
+               type="email" 
+               [(ngModel)]="model.email" 
+               ngControl="email" 
+               name="email"
+               #email="ngModel"
+               required>
+          </p>
+          <div [hidden]="email.valid || email.pristine" class="alert alert-danger">
+            Email is required
+          </div>
+          <p>
+            <button 
+              type="submit" 
+              md-raised-button 
+              color="primary" 
+              [disabled]="!f.form.valid">Submit</button>
+          </p>
+        </form>
+        <pre>this.model = {{ model | json }}</pre>
+      </md-card-content>
+    </md-card>
+  `
+})
+export class Contact {
+  model = {};
+
+  ngOnInit() {
+    console.log('hello `Contact` component');
+  }
+
+  onSubmit(value) {
+    console.log(`Submitted: ${JSON.stringify(value)}`);
+  }
+}
